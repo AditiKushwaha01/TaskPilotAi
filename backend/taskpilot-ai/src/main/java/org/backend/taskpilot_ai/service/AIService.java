3340d9ddc957e456187345d26761fd71cc34a61f
Aditi Kushwaha <137718055+AditiKushwaha01@users.noreply.github.com>
2026-03-30 23:14:26 +0530

    one chane in aiservice

--- backend/taskpilot-ai/src/main/java/org/backend/taskpilot_ai/service/AIService.java
+++ backend/taskpilot-ai/src/main/java/org/backend/taskpilot_ai/service/AIService.java
@@ -7,6 +7,7 @@ import org.backend.taskpilot_ai.dto.TaskDTO;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
